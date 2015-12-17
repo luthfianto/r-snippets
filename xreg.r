b12dfb7aea349d4317ed683bcaf54a8990e0b59b
Rizky Luthfianto <mrluthfianto@gmail.com>
2015-12-18 01:07:06 +0700

    Update xreg.r

--- xreg.r
+++ xreg.r
@@ -1,6 +1,6 @@
-// Snippets for building External Regressors
+# Snippets for building External Regressors
 
-// Create a vector for a holiday
+# Create a vector for a holiday
 flag <- function(nth_day,days_in_year=365) {
     zeroes=rep(0,365)
     zeroes[nth_day]=1
