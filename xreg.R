782e92d978f51fa60ed33f5fac952af114a78cff
Rizky Luthfianto <mrluthfianto@gmail.com>
2016-01-17 23:26:39 +0700

    fix(xreg.R): get_holiday_xreg

--- xreg.R
+++ xreg.R
@@ -7,8 +7,10 @@ get_holiday_vector <- function(nth_day, days_in_year=365) {
   return(zeroes)
 }
 
+# Create a holiday xreg matrix from a `holiday_yday` list
 get_holiday_xreg <- function(holiday_yday) {
-  Map(holiday_vector, holiday_yday)
+  simplify2array( Map(get_holiday_vector, holiday_yday) )
+  # simplify2array return a matrix from Map's list of list
 }
 
 holidays = c("1-1-2014", "14-01-2014", "17-01-2014", "31-01-2014", "01-02-2014", "01-05-2014", "13-05-2014", "15-07-2014", "28-07-2014", "29-07-2014", "31-08-2014", "01-09-2014", "16-09-2014", "05-10-2014", "06-10-2014", "22-10-2014", "25-10-2014", "25-12-2014")
